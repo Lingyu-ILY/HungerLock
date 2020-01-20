56836844d51c48eeb52bf4481b281d35e5c9ea7f
Lingyu-ILY <59048645+Lingyu-ILY@users.noreply.github.com>
2020-01-20 22:37:57 +0800

    Add files via upload

---
+++ src/com/Lingyu/HungerLock/Main.java
@@ -0,0 +1,47 @@
+package com.Lingyu.HungerLock;
+
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+
+import java.text.DecimalFormat;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.FoodLevelChangeEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.*;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	// Fired when plugin is enabled
+    @Override
+    public void onEnable() {
+    	PluginManager pm = this.getServer().getPluginManager();
+    	pm.registerEvents(this, this);
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+    	getServer().getConsoleSender().sendMessage("HungerLock is disable!");
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onFoodChange(FoodLevelChangeEvent event) {
+    	Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            @Override
+            public void run() {
+            	Player player = (Player) event.getEntity();
+            	float st = player.getSaturation();
+            	String message = "§6§l飽和: §e§l" + (new DecimalFormat("0").format(st));
+            if(player.getFoodLevel() > 19) {
+            	event.setCancelled(true);
+                player.setFoodLevel(19);
+                player.spigot().sendMessage(ChatMessageType.ACTION_BAR, TextComponent.fromLegacyText(message));
+            }
+            }
+        }, 1L);
+    }
+}
